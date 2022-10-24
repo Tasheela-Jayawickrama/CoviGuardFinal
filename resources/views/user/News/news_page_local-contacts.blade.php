962468737189bca5eb18410e51a75053447c2771
Tasheela Jayawickrama <tasheelajayawickrama1999@gmail.com>
2022-10-24 07:26:38 +0530

    2022-10-24

--- resources/views/user/News/news_page_local-contacts.blade.php
+++ resources/views/user/News/news_page_local-contacts.blade.php
@@ -1,13 +1,3 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
 <div class="news-contacts-outer" style="border: 1px solid black; padding: 10px; height: 400px; border-radius: 5px">
     <div class="alert alert-light border-dark">contact us</div>
     <div class="alert alert-light border-dark">contact us</div>
@@ -16,5 +6,3 @@
     <div class="alert alert-light border-dark">contact us</div>
 
 </div>
-</body>
-</html>
